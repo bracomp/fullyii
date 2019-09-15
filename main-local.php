4c05b1b9126dbd51759860a136fd5384580e42a2
bracomp <32587250+bracomp@users.noreply.github.com>
2019-09-15 09:34:31 +0500

    Create main-local.php

---
+++ main-local.php
@@ -0,0 +1,16 @@
+<?php // этот файл скопируйте в common/config
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2-start',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+   ],
+];
